5a2715ca43efa3b2ad81fd79739c1eaf3a670652
takosama <takosama.312@gmail.com>
2023-03-21 00:05:00 +0900

    add decode

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.RinJapaneseTokenizer import RinJapaneseTokenizer
 
 if __name__ == '__main__':
-    text = "このトークンライブラリは、漢字を部首に分解してトークン化しています。"
+    text = "凜このトークンライブラリは、漢字を部首に分解してトークン化しています。"
     rin_tokenizer = RinJapaneseTokenizer()
     print(rin_tokenizer.tokenize(text))
+    print(rin_tokenizer.decode(rin_tokenizer.tokenize(text)))
 
 #todo tokenizeの部首取得を少し直す
